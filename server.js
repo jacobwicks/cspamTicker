6ae6dfbe1b7d6dd1ce04f70adeebb07286412db7
jacobwicks <jacob.m.wicks@gmail.com>
2020-09-15 00:22:17 -0700

    port

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const port = process.env.PORT || 8080;
 connect()
   .use(serveStatic(__dirname + "/public"))
   .listen(port, function () {
-    console.log("Server running on port...");
+    console.log(`Server running on port ${port}...`);
   });
